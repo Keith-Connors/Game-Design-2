5dcd3a8f2148f4a77ac44a49cc47f3a8f0b1b25e
Keith Connors <c17507803@mydit.ie>
2018-11-08 18:33:50 +0000

    Camera Movement
    Simple Camera movement script -  Testing how the map looked to the player when camera is in motion, it did not look pixilated.

---
+++ GeoED/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) // Left and Right movement
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime, 0f, 0f)); //Movement is only effected by the X
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) // Up and Down movement
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime, 0f)); //Movement is only effected by the Y
+        }
+    }
+}
