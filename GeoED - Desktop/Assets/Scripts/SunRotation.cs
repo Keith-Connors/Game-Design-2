21d3a19c6ac405b8d9840f8415f6cf6eb22d3e3d
Keith Connors <c17507803@mydit.ie>
2018-11-08 23:27:36 +0000

    Sun Rotation + House cleaning
    a 'sun' will rotate above the continents, giving the impression of faux sunlight.
    
    Some house cleaning done, tidy Project.

---
+++ GeoED - Desktop/Assets/Scripts/SunRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotation : MonoBehaviour
+{
+    public float angle;
+    public GameObject pivotPos;
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up , angle);
+    }
+}
