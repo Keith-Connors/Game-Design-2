d1365ee6df9a8a92b94a53841654647b3c372d56
Keith Connors <c17507803@mydit.ie>
2018-11-18 23:47:51 +0000

    Mouse Clicking added, version is untested.
    JetBrains Rider IDE was used that's why the additional folder is present.
    UI has been redesigned and will be finished at a later time.
    
    South America polished is the first priority for testing.

---
+++ GeoED - Desktop/Assets/Scripts/MouseClickIntro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MouseClickIntro : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Mousebutton zero is the left mouse button.
+        {
+            RaycastHit hit; 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    ClickObj(hit.transform.gameObject);
+                }
+            }
+        }
+    } 
+
+    private void ClickObj(GameObject go)
+    {
+        if (go.tag == "Continent")
+        {
+            SceneManager.LoadScene("Map");
+        }
+        else
+        {
+            print(go);
+        }
+    }
+}
---
+++ GeoED - Desktop/Assets/Scripts/MouseClickMap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MouseClickMap : MonoBehaviour
+{
+
+    public Canvas SouthAmerica;
+    public Canvas InDevelopment;
+
+    private void Start()
+    {
+        SouthAmerica.enabled = false;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Mousebutton zero is the left mouse button.
+        {
+            RaycastHit hit; 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    ClickObj(hit.transform.gameObject);
+                }
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SouthAmerica.enabled = false;
+        }
+    } 
+
+    private void ClickObj(GameObject continent)
+    {
+        if (continent.tag == "Continent")
+        {
+            SouthAmerica.enabled = true;
+        }
+        else
+        {
+            print("Nothing was clicked or is untagged as a continent!");
+        }
+    }
+}
