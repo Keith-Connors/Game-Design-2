29e794a1213d8c420026a210dac3b2d371ba5472
Keith Connors <c17507803@mydit.ie>
2018-11-19 13:23:17 +0000

    Incomplete update.
    Update needs to be finished at a later time, need to go to work.
    Update contains UI and Clicking, although they don't work yet
    - Needed to redo it all since the conflict.

---
+++ GeoED - Desktop/Assets/Scripts/MouseClick_MAP.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class MouseClick_MAP : MonoBehaviour
+{
+    private Canvas _SouthAmerica;
+
+    private void Start()
+    {
+        //SouthAmerica = GetComponent<Canvas>();
+        _SouthAmerica.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    //Do stuff
+                }
+            }
+        }
+    }
+
+    private void CheckObj(GameObject go)
+    {
+        if (go.name == "SouthAmerica" || go.tag == "SouthAmerica")
+        {
+            _SouthAmerica.enabled = true;
+        }
+    }
+}
