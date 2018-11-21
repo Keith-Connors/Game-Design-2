934d188b6c705423e73e1e77394f77781d260e2d
Keith Connors <c17507803@mydit.ie>
2018-11-21 22:49:09 +0000

    Clicking fixed, UI implemented.
    I was going to add a shader to the outline of the selected continent, most likely wont be implemented soon.
    All tags are made, although set to "Construction".

--- GeoED - Desktop/Assets/Scripts/MouseClick_MAP.cs
+++ GeoED - Desktop/Assets/Scripts/MouseClick_MAP.cs
@@ -5,24 +5,85 @@ using UnityEngine;
 
 public class MouseClick_MAP : MonoBehaviour
 {
-    public Canvas SouthAmerica;
+    public Canvas SouthAmerica, NorthAmerica, Europe, Asia, Australia, Africa, Construction;
+
+    private void Start()
+    {
+        SouthAmerica.enabled = false;
+        NorthAmerica.enabled = false;
+        Europe.enabled = false;
+        Asia.enabled = false;
+        Australia.enabled = false;
+        Africa.enabled = false;
+        Construction.enabled = false;
+    }
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SouthAmerica.enabled = false;
+            NorthAmerica.enabled = false;
+            Europe.enabled = false;
+            Asia.enabled = false;
+            Australia.enabled = false;
+            Africa.enabled = false;
+            Construction.enabled = false;
+        }
+        
+        
+        if(Input.GetMouseButtonDown(0))
+        {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit, 100f))
-        {
-            if (hit.transform != null)
+            if (Physics.Raycast(ray, out hit, 100f))
             {
-                obj(hit.transform.gameObject);
+                if (hit.transform != null)
+                {
+                    obj(hit.transform.gameObject);
+                }
             }
         }
     }
 
     private void obj(GameObject go)
     {
-        print(go.name);
+        print("User has clicked on continent: "+go.name);
+        
+        if (go.tag == "SouthAmerica")
+        {
+            SouthAmerica.enabled = true;
+        }
+        
+        if (go.tag == "Asia")
+        {
+            Asia.enabled = true;
+        }
+        
+        if (go.tag == "Europe")
+        {
+            Europe.enabled = true;
+        }
+        
+        if (go.tag == "NorthAmerica")
+        {
+            NorthAmerica.enabled = true;
+        }
+        
+        if (go.tag == "Australia")
+        {
+            Australia.enabled = true;
+        }
+        
+        if (go.tag == "Africa")
+        {
+            Africa.enabled = true;
+        }
+        
+        if (go.tag == "Construction")
+        {
+            Construction.enabled = true;
+        }
     }
 }
---
+++ GeoED - Desktop/Assets/Shaders/Shader_Outline.shader
@@ -0,0 +1,82 @@
+﻿Shader "GeoED_SHADERS/Shader_Outline"
+{
+	Properties
+	{
+	    _mainColor ("Main Color", Color) = (0.5, 0.5, 0.5, 1)
+		_mainTex ("Texture", 2D) = "white" {}
+		_OutlineColor("Outline Color", Color) = (0, 0, 0, 1)
+		_OutlineWidth("Outline Width", Range(1.0, 5.0)) = 1.01
+	}
+	
+	CGINCLUDE
+	#include "UnityCG.cginc"
+	
+	struct appdata
+	{
+	    float4 vertex : POSITION;
+	    float3 normal : NORMAL;
+	};
+	
+	struct v2f
+	{
+	    float4 pos : POSITION;
+	    float4 color : COLOR;
+	    float3 normal : NORMAL;
+	};
+	
+	float _OutlineWidth;
+	float4 _OutlineColor;
+	
+	v2f vert(appdata v)
+	{
+	    v.vertex.xyz *= _OutlineWidth;
+	    v2f o;
+	    o.pos = UnityObjectToClipPos(v.vertex);
+	    o.color = _OutlineColor;
+	    return o;
+	}
+	
+	ENDCG
+	
+	SubShader
+	{
+	    Pass //Outline Render Pass
+	    {
+	        ZWrite Off
+	        
+	        CGPROGRAM
+	        #PRAGMA vertex vert;
+	        #PRAGMA fragment frag;
+	        
+	        half4 frag(v2f i) : COLOR
+	        {
+	        return _OutlineColor;
+	        }
+	        ENDCG
+	    }
+	    
+	    Pass //Render Normals
+	    {
+	        ZWrite On
+	        
+	        Material
+	        {
+	            Diffuse[_Color]
+	            Ambient[_Color]
+	        }
+	        
+	        Lighting On
+	        
+	        SetTexture[_mainTex]
+	        {
+	            ConstantColor[_Color]
+	        }
+	        
+	        SetTexture[_mainTex]
+	        {
+	            Combine previous * primary DOUBLE
+	        }
+	    }
+	}
+}
+		
