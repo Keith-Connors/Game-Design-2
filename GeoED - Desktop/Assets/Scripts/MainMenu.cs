39e627fc15ab866034962e087e877f4cc837cb83
Keith Connors <c17507803@mydit.ie>
2018-11-29 11:43:57 +0000

    MainMenu added

---
+++ GeoED - Desktop/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void ChangeSceneToMAP()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void exitGame()
+    {
+        
+    }
+}
