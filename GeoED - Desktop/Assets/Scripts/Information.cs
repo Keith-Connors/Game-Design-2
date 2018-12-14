f59f379448e78a9103196fd37dfb1940183d46ce
Keith Connors <c17507803@mydit.ie>
2018-12-14 00:00:55 +0000

    whatever was changed

---
+++ GeoED - Desktop/Assets/Scripts/Information.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Information : MonoBehaviour
+{
+
+    public Canvas CanvasToShow;
+
+    private void Start()
+    {
+        CanvasToShow.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (CanvasToShow != null)
+        {
+            
+        }
+    }
+}
